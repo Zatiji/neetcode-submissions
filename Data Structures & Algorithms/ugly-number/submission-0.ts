56b00117eccb5f7eba7ac4192a47e47270139a46
David M. <103978628+Zatiji@users.noreply.github.com>
2026-08-20 20:13:15 -0400

    Add: ugly-number - submission-0

---
+++ Data Structures & Algorithms/ugly-number/submission-0.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {boolean}
+     */
+    isUgly(n: number): boolean {
+        if (n <= 0) {
+            return false;
+        }
+
+        while (n > 1) {
+            if(n % 2 === 0) {
+                n = n/2;
+                continue;
+            } else if (n % 3 === 0) {
+                n = n/3;
+                continue;
+            } else if (n % 5 === 0) {
+                n = n/5;
+                continue;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
